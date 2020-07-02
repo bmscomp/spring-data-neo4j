3c1fbadaf8e33becf78a290d6f90bdc227b8b796
Michael Simons <michael@simons.ac>
2020-07-02 12:31:44 +0200

    Fix license header.

--- spring-data-neo4j/src/main/java/org/neo4j/springframework/data/repository/event/package-info.java
+++ spring-data-neo4j/src/main/java/org/neo4j/springframework/data/repository/event/package-info.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * Contains the infrastructure for the event system. The event system comes in two flavours: Events that are based
  * on Spring's application event system and callbacks that are based on Spring Data's callback system. Application
